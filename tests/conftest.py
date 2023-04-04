b0fe92c4680744e74a16cf69a6477905a1d012de
Cariad Eccleston <cariad@cariad.earth>
2023-04-04 11:03:06 +0100

    Initial implementation (#2)

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+from csv import reader
+from pathlib import Path
+from typing import Callable, Iterable, Tuple
+
+from pytest import fixture
+
+from checksome.logging import logger
+
+logger.setLevel("DEBUG")
+
+
+@fixture
+def data() -> Path:
+    return Path(__file__).parent / "data"
+
+
+@fixture
+def load_ranges(
+    data: Path,
+) -> Callable[[str], Iterable[Tuple[int, int, bytes]]]:
+    def load(filename: str) -> Iterable[Tuple[int, int, bytes]]:
+        with open(data / filename) as f:
+            for row in reader(f):
+                yield (
+                    int(row[0]),
+                    int(row[1]),
+                    bytes.fromhex(row[2]),
+                )
+
+    return load
